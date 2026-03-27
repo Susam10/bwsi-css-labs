12149fa7f24574785974ee914d6d2cf1d19d5fd4
Susam10 <92957162+Susam10@users.noreply.github.com>
2026-03-27 16:42:55 -0400

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "SAMARTH BRIGHT" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-    #YAWN
+    
 if __name__ == "__main__":
     main()
